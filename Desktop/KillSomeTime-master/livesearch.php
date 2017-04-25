486f03fe4c8e3e7f15eddd249a1bca7917995701
shiek mohamed <usacricketers@gmail.com>
2017-04-25 19:38:23 -0400

    first commit

---
+++ Desktop/KillSomeTime-master/livesearch.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html" charset="utf-8" />
+</head>
+
+<body>
+
+<form action="index.php" method="post">
+    <input type="text" name="search"placeholder="search for games"
+    input type="submit" value=">>" />
+    
+    
+    </form>
+    
+</body>
+</html>
